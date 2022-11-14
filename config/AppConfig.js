161f45c3cdaabfaf18e1e882255e35f555fb9a60
kamcod <mkamran4786@gmail.com>
2022-11-14 17:46:21 +0500

    - done responsiveness of profile pic and background

---
+++ config/AppConfig.js
@@ -0,0 +1,6 @@
+const AppConfig = {
+  appUrl: 'http://localhost:3000/',
+  apiUrl: 'http://localhost:3000/',
+
+};
+export default AppConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
-import React from "react";
-import HomePage from "./HomePage/HomePage";
+import React, {useEffect, useState} from "react";
+import Image from "next/image";
+import AppConfig from "../config/AppConfig";
 
 export default function Home() {
+    const [dpWidth, setDpWidth] = useState(100);
+    const [dpHeight, setDpHeight] = useState(100);
+
+    const imgLoader = ({ src, width, quality }) => {
+        return `${AppConfig.appUrl}/${src}?w=${width}&q=${quality || 75}`
+    }
+
+    useEffect(()=> {
+        const vWidth = window.innerWidth;
+        let value = vWidth * 0.32;
+        setDpWidth(value);
+        setDpHeight(value);
+    }, []);
+
   return (
     <>
-      <HomePage />
+        <div className="gray_layer">
+        </div>
+        <Image className="dp" src="/images/bg.jpg" width={dpWidth} loader={imgLoader} height={dpHeight} alt="profile photo"/>
     </>
   )
 }
